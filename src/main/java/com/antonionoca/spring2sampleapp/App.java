dfbccd0fc910d47b78cb1445e5fb53dd27b83a5f
Antonio Noca <antonionoca@gmail.com>
2017-08-30 18:14:19 +0100

    Codacy comments

--- src/main/java/com/antonionoca/spring2sampleapp/App.java
+++ src/main/java/com/antonionoca/spring2sampleapp/App.java
@@ -1,6 +1,5 @@
 package com.antonionoca.spring2sampleapp;
 
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class App  {
