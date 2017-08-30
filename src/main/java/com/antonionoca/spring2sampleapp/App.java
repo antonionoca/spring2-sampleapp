14f3e6f3a1dee68d2f6927543917e756bd3dc53d
Antonio Noca <antonionoca@gmail.com>
2017-08-30 18:16:52 +0100

    Introducing logs with SLF4J

--- src/main/java/com/antonionoca/spring2sampleapp/App.java
+++ src/main/java/com/antonionoca/spring2sampleapp/App.java
@@ -1,15 +1,19 @@
 package com.antonionoca.spring2sampleapp;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class App  {
 
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
+
     public static void main(String[] args) {
 
-        System.out.println("Starting application..");
+        logger.info("Starting application..");
 
         new FileSystemXmlApplicationContext("classpath:beans.xml");
 
-        System.out.println("Application Context loaded..");
+        logger.info("Application Context loaded..");
     }
 }
