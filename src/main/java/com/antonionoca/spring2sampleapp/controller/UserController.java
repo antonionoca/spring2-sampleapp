d1cbd4ef4e68cdf1d571780303af23d4142a710e
Antonio Noca <antonionoca@gmail.com>
2017-09-02 07:56:55 +0100

    Fixed TODOs on web.xml
    Updated user bean.
    Added dummy code to userController.
    Using custom beans.xml and application context.

--- src/main/java/com/antonionoca/spring2sampleapp/controller/UserController.java
+++ src/main/java/com/antonionoca/spring2sampleapp/controller/UserController.java
@@ -1,4 +1,27 @@
 package com.antonionoca.spring2sampleapp.controller;
 
-public class UserController {
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserController extends AbstractController {
+
+    public UserController() {
+
+    }
+
+    protected ModelAndView handleRequestInternal(
+            HttpServletRequest request, HttpServletResponse response)
+            throws Exception {
+
+        List<String> users = new ArrayList<String>();
+        users.add("Joe");
+
+        return new ModelAndView("home", "users", users);
+    }
 }
